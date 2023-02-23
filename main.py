44925c9e589c1f98968f4f78a5a84022cc511ffc
ingold <ingoldaguilar45@gmail.com>
2023-02-22 18:28:54 -0600

    Some changes on the functions and the base program

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ def create_db():
     cursor = connection.cursor()
 
     try:
+        # possible changes -> task_id
+        cursor.execute("""
+        CREATE TABLE categories(
+                cat_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name VARCHAR(100) UNIQUE NOT NULL
+        ) 
+        """)
+    except sqlite3.OperationalError:
+        print("The table 'categories' already exist.")
+    else:
+        print("Table 'categories' created successfully.")
+
+    try:
         cursor.execute("""
         CREATE TABLE tasks(
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 name VARCHAR(100) UNIQUE NOT NULL,
-                description VARCHAR(1000) NOT NULL
+                description VARCHAR(1000) NOT NULL,
+                FOREIGN KEY(id) REFERENCES categories(cat_id)
         ) 
         """)
     except sqlite3.OperationalError:
@@ -23,25 +37,29 @@ def create_db():
     else:
         print("Table 'tasks' created successfully.")
 
-    try:
-        # possible changes -> task_id
-        cursor.execute("""
-        CREATE TABLE users(
-                id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                name VARCHAR(100) UNIQUE NOT NULL, 
-                task_id INTEGER NOT NULL, 
-                FOREIGN KEY(task_id) REFERENCES tasks(id)
-        ) 
-        """)
-    except sqlite3.OperationalError:
-        print("The table 'users' already exist.")
-    else:
-        print("Table 'users' created successfully.")
-
     # close the connection
     connection.close()
 
 # add category
+def add_category():
+    category = input("Name of the new category\n>")
+
+    connection = sqlite3.connect("toDo.db")
+    cursor = connection.cursor()
+
+    try:
+        # insert the new category
+        cursor.execute(f"INSERT INTO categories VALUES(null, '{category}') ")
+    except sqlite3.IntegrityError:
+        print(f"Error: The category '{category}' already exist.")
+    else:
+        print(f"Category '{category}' created successfully.")
+
+    # save and close
+    connection.commit()
+    connection.close()
+
+# add task
 def add_task():
     task_name = input("Name of the new task\n>")
     description = input(f"Insert a description for the task {task_name}\n>")
@@ -50,30 +68,12 @@ def add_task():
     cursor = connection.cursor()
 
     try:
-        # insert the new category
+        # insert the new task
         cursor.execute(f"INSERT INTO tasks VALUES(null, '{task_name}', '{description}') ")
     except sqlite3.IntegrityError:
         print(f"Error: The task '{task_name}' already exist.")
     else:
         print(f"Task '{task_name}' created successfully.")
-
-    # save and close
-    connection.commit()
-    connection.close()
-
-# add dish
-def add_user():
-    connection = sqlite3.connect("toDo.db")
-    cursor = connection.cursor()
-
-    # get the dish
-    user = input("Name of the new user\n>")
-    try:
-        cursor.execute(f"INSERT INTO users VALUES(null, '{user}', {user_id}) ")
-    except sqlite3.IntegrityError:
-        print(f"Error: The user '{user}' already exist.")
-    else:
-        print(f"User '{user}' created successfully.")
 
     # save and close
     connection.commit()
