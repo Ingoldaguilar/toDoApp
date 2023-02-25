2279a1fe0d453689a2aa4fa766453b8dc680edae
ingold <ingoldaguilar45@gmail.com>
2023-02-24 19:42:46 -0600

    Create the delete task function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # imports
 import sqlite3
-from tkinter import *
+# from tkinter import *
 
 # ---------------- Functions ----------------
 # create db
@@ -91,6 +91,34 @@ def add_task():
     connection.commit()
     connection.close()
 
+
+def delete_task():
+
+    connection = sqlite3.connect("toDo.db")
+    cursor = connection.cursor()
+
+    # show categories to the user
+    tasks = cursor.execute("SELECT * FROM tasks").fetchall()
+
+    print("\t||Tasks||")
+    for task in tasks:
+        print(f">{task[1]}") # name
+
+    task_name = input("Insert the name of the task you want to delete\n>")
+
+    # Reminder: capture the error when delete a non-existing table
+    try:
+        # insert the new task
+        cursor.execute(f"DELETE FROM tasks WHERE name='{task_name}'")
+    except sqlite3.OperationalError:
+        print(f"Error: The task '{task_name}' doesn't exist.")
+    else:
+        print(f"Task '{task_name}' deleted successfully.")
+
+    # save and close
+    connection.commit()
+    connection.close()
+
 # show menu
 def show_tasks():
 
@@ -111,41 +139,44 @@ def show_tasks():
 
 # ---------------- GUI ----------------
 # root
-root = Tk()
-root.title("To Do App")
-root.iconbitmap("toDoIcon.ico")
-root.resizable(False, False)
-root.geometry("500x500")
+#root = Tk()
+#root.title("To Do App")
+#root.iconbitmap("toDoIcon.ico")
+#root.resizable(False, False)
+#root.geometry("500x500")
 
 
  # Buttons
-Button(text="Exit", command=root.quit).pack()
+#Button(text="Exit", command=root.quit).pack()
 
 # mainloop
-root.mainloop()
+#root.mainloop()
 # ---------------- End GUI ----------------
 
 # ---------------- Console ----------------
 # create_db() database created.
 
 # show menu
-#while True:
-    #print("\nWelcome to the To Do App")
-    #option = input("\nInsert a option:\n[1] Add a category\n[2] Add a task\n[3] Show tasks\n[4] Exit\n>")
+while True:
+    print("\nWelcome to the To Do App")
+    option = input("\nInsert a option:\n[1] Add a category\n[2] Add a task\n[3] Delete a task\n[4] Show tasks\n[5] Exit\n>")
 
-    #if option == "1":
-        #add_category()
+    if option == "1":
+        add_category()
 
-    #elif option == "2":
-        #add_task()
+    elif option == "2":
+        add_task()
 
-    #elif option == "3":
-        #show_tasks()
+    elif option == "3":
+        delete_task()
 
-    #elif option == "4":
-        #print("Bye!")
-        #break
+    elif option == "4":
+        show_tasks()
 
-    #else:
-        #print("Error: select a valid option")
+    elif option == "5":
+        print("Bye!")
+        break
+
+    else:
+        print("Error: select a valid option")
 # ---------------- End Console ----------------
